c5c52961839bd8308ce722546cea6fbfaa0ba206
MatheoMONSTER <81566430+MatheoMONSTER@users.noreply.github.com>
2023-07-26 23:22:41 +0200

    Changed user model values

--- LibraryManager/Models/User.cs
+++ LibraryManager/Models/User.cs
@@ -8,6 +8,12 @@ namespace LibraryManager.Models
         public int Id { get; set; }
         [Required]
         public string Username { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required] 
+        public string LastName { get; set; }
+
         [Required]
         public string Email { get; set; }
     }
